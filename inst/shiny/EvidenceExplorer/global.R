2a49af50c75e6546a259189584a5f554d2221868
Seonji Kim <sunji0316@hanmail.net>
2023-07-10 13:51:54 +0900

    Delete priscription condition of PPI

--- inst/shiny/EvidenceExplorer/global.R
+++ inst/shiny/EvidenceExplorer/global.R
@@ -1,11 +1,19 @@
 source("DataPulls.R")
 source("PlotsAndTables.R")
 
-shinySettings <- list(dataFolder = "./shinyData", blind = FALSE)
+shinySettings <- list(dataFolder = "./ShinyData", blind = FALSE)
 dataFolder <- shinySettings$dataFolder
 blind <- shinySettings$blind
 connection <- NULL
 positiveControlOutcome <- NULL
+
+#database_cdmpv531<- readRDS("D:/git/DdiPpiCloNoGenericV5/inst/shiny/EvidenceExplorer/ShinyData/database_cdmpv531.rds")
+#`database_Meta-analysis` <- readRDS("D:/git/DdiPpiCloNoGenericV5/inst/shiny/EvidenceExplorer/ShinyData/database_Meta-analysis.rds")
+#`database_Meta-analysis`$vocabulary_version <- database_cdmpv531$vocabulary_version
+#`database_Meta-analysis`$min_obs_period_date <- database_cdmpv531$min_obs_period_date
+#`database_Meta-analysis`$max_obs_period_date <- database_cdmpv531$max_obs_period_date
+#`database_Meta-analysis`$study_package_version <- database_cdmpv531$study_package_version
+#saveRDS(file = "./ShinyData/database_Meta-analysis.rds", object = `database_Meta-analysis`)
 
 splittableTables <- c("covariate_balance", "preference_score_dist", "kaplan_meier_dist")
 
